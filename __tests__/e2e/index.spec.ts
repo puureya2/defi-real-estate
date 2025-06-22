79a0dd22efd5d7e84f02ffbffbde54d01c33cbc1
Kevin Chifamba <63073697+puureya2@users.noreply.github.com>
2025-06-23 01:31:31 +0800

    configured playwright github actions

---
+++ __tests__/e2e/index.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('homepage redirects to /settings', async ({ page }) => {
+    await page.goto('/'); // remember -- we don't have to provide full URL as we already set baseUrl in playwright config file
+    await expect(page).toHaveURL('/settings');
+});
