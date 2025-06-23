56d0f568a842ca387d9b30b3e67e501292adc25f
Kevin Chifamba <63073697+puureya2@users.noreply.github.com>
2025-06-23 14:13:47 +0800

    test CI/CD 17 (jest alone)

--- __tests__/unit/demo.test.jsx
+++ __tests__/unit/demo.test.jsx
@@ -1,12 +1,12 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
-// ✅ mock RainbowKit
-jest.mock('@rainbow-me/rainbowkit', () => ({
-  ConnectButton: () => <div>Mock Connect Button</div>,
-}));
+// // ✅ mock RainbowKit
+// jest.mock('@rainbow-me/rainbowkit', () => ({
+//   ConnectButton: () => <div>Mock Connect Button</div>,
+// }));
 
-import Home from '../../src/pages/index';
+import { Home } from '../../src/pages/index';
 
 describe('Home', () => {
   it('renders a heading', () => {
