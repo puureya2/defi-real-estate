049792c7f2e89bc1b86af5eaf85fe82ce5f62796
Kevin Chifamba <63073697+puureya2@users.noreply.github.com>
2025-06-23 09:48:48 +0800

    test CI/CD 2

--- __tests__/e2e/index.spec.ts
+++ __tests__/e2e/index.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('homepage redirects to /settings', async ({ page }) => {
     await page.goto('/'); // remember -- we don't have to provide full URL as we already set baseUrl in playwright config file
-    await expect(page).toHaveURL('/settings');
+    await expect(page).toHaveURL('/settings!');
 });
